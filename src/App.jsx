afd3fc32f24321940bd9fdc5838dbc294b256ade
Owadokun Samuel <sammy.owadokun@gmail.com>
2026-05-02 03:04:38 +0100

    effect changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import ScrollToTop from "./components/layout/ScrollToTop.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 export default function App() {
   return (
