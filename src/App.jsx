b865851fdaec91c9a14d9742d075573487d4248e
Owadokun Samuel <sammy.owadokun@gmail.com>
2026-05-02 02:53:35 +0100

    final fix: ScrollToTop import with .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import ScrollToTop from "./components/layout/ScrollToTop.jsx";
-
+import ScrollToTop from "@/components/layout/ScrollToTop.jsx";
 export default function App() {
   return (
     <>
