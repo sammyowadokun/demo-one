87d9fec1168f0dd22b7bd45bae1992689372eb86
Owadokun Samuel <sammy.owadokun@gmail.com>
2026-05-02 02:45:45 +0100

    fix: add .jsx extension to ScrollToTop import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import ScrollToTop from "./components/layout/ScrollToTop";
+import ScrollToTop from "./components/layout/ScrollToTop.jsx";
 
 export default function App() {
   return (
