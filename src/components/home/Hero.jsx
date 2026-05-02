9eb5cd7784ae06cf7d5199139d96047958cb57db
Owadokun Samuel <sammy.owadokun@gmail.com>
2026-05-02 04:09:51 +0100

    make all changes

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import img from '../../assets/images/pharmacy.jpg';
+import img from '../../assets/Images/pharmacy.jpg';
 
 export default function Hero() {
   return (
